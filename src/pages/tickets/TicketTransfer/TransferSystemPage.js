76da0a04e7a4ee140ba03151f9f9c042a5e815df
DLaraND <carlos.lara@nuestrodario.com.gt>
2020-08-21 20:41:16 -0600

    tickets system trasfer

--- src/pages/tickets/TicketTransfer/TransferSystemPage.js
+++ src/pages/tickets/TicketTransfer/TransferSystemPage.js
@@ -1,7 +1,7 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Layaout from '../../parcials/Layaout';
 import CardHeader from '../../../components/CardHeader'
-import { getTicketsSystemTransfer, getStore } from '../../../functions/ticketFunction';
+import { storeTicketsSystemTransfer, getTicketsSystemTransfer, getStore } from '../../../functions/ticketFunction';
 import Select from 'react-select';
 import {
     MDBRow,
@@ -9,51 +9,132 @@ import {
     MDBInput,
     MDBBtn,
     MDBIcon,
-    MDBAnimation
+    MDBAnimation,
+    MDBContainer,
+    MDBTypography,
+    MDBCard,
+    MDBCardBody
 } from 'mdbreact';
 
 const TransferSystemPage = () => {
-    const [dataStores, setdataSores] = useState(null)
+    const [dataStores, setdataSores] = useState([]);
+    const [fields, setFields] = useState([{ upc: null, alu: null, talla: null, factura: null, store: null }]);
+    let storesList = [];
+    let ticketsList = [];
 
-    useEffect(() => {
-        getStore().then((res)=>{ setdataSores(res) });
-    },[]);
-    console.log(dataStores)
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
+    getTicketsSystemTransfer().then((resp) => { resp.map(res => ticketsList.push(res)); console.log(ticketsList)});
+    getStore().then((resp) => { resp.map(res => storesList.push({ value: res.name, label: res.name })) });
+
+    function crearTicket() {
+        alert("Creado")
+        storeTicketsSystemTransfer(fields).then(response => {
+            console.log(response)
+        }).catch(err => {
+            alert("Error")
+        })
+    }
+
+    //created input
+    function handleChange(i, event, name) {
+        const values = [...fields];
+        if (name == "store") {
+            values[i][name] = event.value;
+        } else if (event.target.value == "") {
+            values[i][name] = null;
+        } else {
+            values[i][name] = event.target.value;
+        }
+        setFields(values);
+    }
+
+    function handleAdd() {
+        const values = [...fields];
+        if (fields.length <= 10) {
+            values.push({ upc: null, alu: null, talla: null, factura: null });
+            setFields(values);
+        } else {
+            alert("Se alconzó el limite de tickets por creación")
+        }
+    }
+
+    function handleRemove(i) {
+        if (i !== 0) {
+            const values = [...fields];
+            values.splice(i, 1);
+            setFields(values);
+        }
+    }
+
     const value = { value: 'Selecciona una tienda', label: 'Selecciona una tienda' };
     return (
         <Layaout>
             <br></br>
             <CardHeader title="Tickets" icon="ticket-alt">
-                <MDBRow style={{ justifyContent: "center", display: "flex" }}>
-                    <MDBCol md='2'>
-                        <MDBInput label='Upc' type='text' validate />
-                    </MDBCol>
-                    <MDBCol md='2'>
-                        <MDBInput label='Alu' type='text' validate />
-                    </MDBCol>
-                    <MDBCol md='2'>
-                        <MDBInput label='Talla' type='text' validate />
-                    </MDBCol>
-                    <MDBCol md='2'>
-                        <MDBInput label='Factura' type='text' validate />
-                    </MDBCol>
-                    <MDBCol md='3' style={{marginTop: "26px"}}>
-                        <Select
-                            defaultValue={value}
-                            options={options}
-                        />
-                    </MDBCol>
-                </MDBRow>
-                <MDBRow style={{ justifyContent: "center", display: "flex" }}>
-                    <MDBBtn color='light-blue'><MDBIcon icon="plus"/> Agregar</MDBBtn>
-                    <MDBBtn color='light-green'><MDBIcon icon='ticket-alt'/>  Crear Ticket</MDBBtn>
+                {fields.map((field, idx) => {
+                    return (
+                        <MDBRow id={idx} className="center-element" key={`${field}-${idx}`}>
+                            <MDBCol md='2'>
+                                <MDBInput label='Upc' type='text' validate onChange={e => handleChange(idx, e, "upc")} />
+                            </MDBCol>
+                            <MDBCol md='2'>
+                                <MDBInput label='Alu' type='text' validate onChange={e => handleChange(idx, e, "alu")} />
+                            </MDBCol>
+                            <MDBCol md='2'>
+                                <MDBInput label='Talla' type='text' validate onChange={e => handleChange(idx, e, "talla")} />
+                            </MDBCol>
+                            <MDBCol md='2'>
+                                <MDBInput label='Factura' type='text' validate onChange={e => handleChange(idx, e, "factura")} />
+                            </MDBCol>
+                            <MDBCol md='3' style={{ marginTop: "26px" }}>
+                                <Select
+                                    onChange={e => handleChange(idx, e, "store")}
+                                    defaultValue={value}
+                                    options={storesList}
+                                />
+                            </MDBCol>
+                            <MDBCol md='1' style={{ paddingLeft: "0px", paddingTop: "20px" }}>
+                                {idx !== 0 && (<MDBBtn size="sm" color='danger' onClick={() => handleRemove(idx)}>X</MDBBtn>)}
+                            </MDBCol>
+                        </MDBRow>
+                    )
+                })}
+                <MDBRow className="center-element">
+                    <MDBBtn color='light-blue' onClick={() => handleAdd()}><MDBIcon icon="plus" /> Agregar</MDBBtn>
+                    <MDBBtn color='light-green' onClick={() => crearTicket()}><MDBIcon icon='ticket-alt' />  Crear Ticket</MDBBtn>
                 </MDBRow>
             </CardHeader>
+            <br></br>
+
+            <MDBRow>
+                <MDBCol md='6' className="center-element">
+                    <MDBTypography tag="h3" variant="h3-responsive"> Tickets Creados </MDBTypography>
+                </MDBCol>
+                <MDBCol md='6' className="center-element">
+                    <MDBTypography tag="h3" variant="h3-responsive"> Tickets Asignados </MDBTypography>
+                </MDBCol>
+            </MDBRow>
+            <br></br>
+            <MDBRow>
+                <MDBCol md="6">
+                    <MDBContainer>
+                        <MDBCard>
+                            <MDBCardBody>
+                                CREADOS
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBContainer>
+                </MDBCol>
+                <MDBCol md="6">
+                    <MDBContainer>
+                        <MDBCard>
+                            <MDBCardBody>
+                                ASINGNADOS
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBContainer>
+                </MDBCol>
+            </MDBRow>
+            <br></br>
         </Layaout>
     )
 }

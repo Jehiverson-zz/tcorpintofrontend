4e4f4fb7778301cd99454ced4236abf050c55bef
DLaraND <carlos.lara@nuestrodario.com.gt>
2021-03-10 01:52:28 -0600

    reporteria v4

--- src/pages/reports/modules.js
+++ src/pages/reports/modules.js
@@ -3,49 +3,49 @@ const modules = [
         _id: "1",
         name: "Bitacora de Ejecución",
         surname: "bitacora",
-        view: "user"
+        view: ["admin","user"]
     },
     {
         _id: "2",
         name: "Venta Diaria",
         surname: "venta_diaria",
-        view: "user"
+        view: ["admin","user"]
     },
     {
         _id: "3",
         name: "Tickets",
         surname: "tickets",
-        view: "user"
+        view: ["admin","user"]
     },
     {
         _id: "4",
         name: "Mercadería Dañada",
         surname: "mercaderia",
-        view: "user"
+        view: ["admin","user"]
     },
     {
         _id: "5",
         name: "Certificados",
         surname: "certificados",
-        view: "user",
+        view: ["admin","user"],
     },
     {
         _id: "6",
         name: "Retiros",
         surname: "retiros",
-        view: "user"
+        view: ["admin","user"]
     },
     {
         _id: "7",
         name: "Colaboradores",
         surname: "colaboradores",
-        view: "admin"
+        view: ["admin"]
     },
     {
         _id: "8",
         name: "Usuarios",
         surname: "usuarios",
-        view: "admin"
+        view: ["admin"]
     },
 ]
 
